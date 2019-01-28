7a20addd82a01a0abaf8e6e40996ab47845183d3
xiekainew <kaixie@newbanker.cn>
2019-01-28 09:59:36 +0800

    udpate electron

---
+++ vue/src/pages/home/home.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        home
+        <el-form>
+            <h-form
+                label="12"
+                require
+                prop="f"
+            ></h-form>
+        </el-form>
+    </section>
+</template>
+
+<script>
+    import HForm from '@xiekai2018/h-form'
+    export default {
+        data() {
+            return {}
+        },
+        components: {
+            HForm
+        },
+        methods: {},
+        mounted() {
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
