28401357df4c9e02049829332042cdd6c73c09f7
xiekainew <kaixie@newbanker.cn>
2019-02-01 10:41:03 +0800

    udpate electron

---
+++ electron/main.js
@@ -0,0 +1,17 @@
+// const electron = require('electron')
+const {app, BrowserWindow} = require('electron')
+let win
+function createWindow () {
+    win = new BrowserWindow({width: 800, height: 600})
+    win.loadFile('index.html')
+    win.webContents.openDevTools()
+    win.on('closed', () => {
+        win = null
+    })
+}
+app.on('ready', createWindow)
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
