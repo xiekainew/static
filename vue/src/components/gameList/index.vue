37210b984641d364aa19032b297f7a4d0628a7ac
xiekainew <15589720577@163.com>
2019-05-07 13:56:39 +0800

    udpate code

---
+++ vue/src/components/gameList/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="game-list"
+        :style="{height: height}"
+    >
+        <div class="left"><span class="iconfont">&#xe661;</span></div>
+        <div class="right"><span class="iconfont">&#xe662;</span></div>
+        <div class="list-item" v-for="n in 9" :keys="n"></div>
+    </div>
+</template>
+<script>
+  export default {
+    name: 'gameList',
+    data() {
+      return {}
+    },
+    props: {
+      height: String
+    },
+    methods: {},
+    mounted() {
+    }
+  }
+</script>
+
+<style lang="scss">
+.game-list{
+  background: rgba(255,0,255, .2);
+  font-size: 0;
+  white-space: nowrap;
+  padding: 0 25px;
+  position: relative;
+  .left, .right{
+    width: 25px;
+    height: 100%;
+    border:1px solid rgba(0,0,0,0.05);
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+    cursor: pointer;
+    background: white;
+  }
+  .left{
+    left: 0;
+  }
+  .right{
+    right: 0;
+  }
+  .list-item{
+    width: 220px;
+    border:1px solid rgba(0,0,0,0.1);
+    display: inline-block;
+    height: 100%;
+  }
+}
+</style>
--- vue/src/pages/home/home.vue
+++ vue/src/pages/home/home.vue
@@ -8,17 +8,22 @@
                 prop="f"
             ></h-form>
         </el-form>
+        <div class="content">
+            <game-list height="130px"></game-list>
+        </div>
     </section>
 </template>
 
 <script>
     import HForm from '@xiekai2018/h-form'
+    import GameList from '@/components/gameList'
     export default {
         data() {
             return {}
         },
         components: {
-            HForm
+            HForm,
+            GameList
         },
         methods: {},
         mounted() {
@@ -27,5 +32,8 @@
 </script>
 
 <style lang="scss">
-
+.content{
+    width: 1200px;
+    margin: 0 auto;
+}
 </style>
