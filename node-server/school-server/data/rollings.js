d57fe435704045133075d28f050eb283ac8ed1f9
xiekainew <15589720577@163.com>
2019-09-24 14:13:36 +0800

    udapte data

--- node-server/school-server/data/rollings.js
+++ node-server/school-server/data/rollings.js
@@ -1,14 +1,28 @@
 module.exports = [
       {
-        "rollingId": "R201909172231498244788",
-        "title": "测试投票",
-        "picture": "1568730688524_beds-4.jpg",
-        "url": "http://szpy.nenu.edu.cn/api/info/I20190916220453104586",
+        "rollingId": "R201909232022094247290",
+        "title": "习近平在中央政协工作会议 暨庆祝中国人民政治协商会议成立70周年大会上发表重要讲话",
+        "picture": "1569241391735_W020190921404172244467.jpg",
+        "url": "http://szpy.nenu.edu.cn/api/info/I201909232015331312298",
         "rollingOrder": 0,
-        "viewTimes": 2,
+        "viewTimes": 0,
         "deleted": 0,
-        "createTime": "2019-09-17 22:31:49",
-        "updateTime": "2019-09-17 22:51:55",
-        "status": "正常"
+        "createTime": "2019-09-23 20:22:09",
+        "updateTime": "2019-09-23 20:22:09",
+        "status": "正常",
+        "pictureUrl": "http://qiniu-sz-training-file.huanxizizai.com/1569241391735_W020190921404172244467.jpg"
+      },
+      {
+        "rollingId": "R201909232024415869784",
+        "title": "学习时报创刊20周年座谈会在中央党校（国家行政学院）举行",
+        "picture": "1569241554064_W020190916794782874950.jpg",
+        "url": "http://szpy.nenu.edu.cn/api/info/I201909232019036359415",
+        "rollingOrder": 0,
+        "viewTimes": 0,
+        "deleted": 0,
+        "createTime": "2019-09-23 20:25:08",
+        "updateTime": "2019-09-23 20:25:08",
+        "status": "正常",
+        "pictureUrl": "http://qiniu-sz-training-file.huanxizizai.com/1569241554064_W020190916794782874950.jpg"
       }
     ]
